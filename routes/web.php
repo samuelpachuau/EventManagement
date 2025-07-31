9d8fe0f57cd61d6646d8fca6f8d40516432298c3
zarmawii <Zarmoyee07@gmail.com>
2025-07-31 23:36:34 +0530

    Models and migrations

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\AuthManager;
 use App\Http\Controllers\EventController;
 use App\Http\Controllers\TicketController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\EventController;
+
 
 Route::get('/', function () {
     return view('index');
@@ -15,3 +15,5 @@ Route::post('login', [AuthManager::class, 'loginPost'])->name('login.post');
 
 Route ::get("register",[AuthManager::class,"register"])->name("register");
 Route ::post("register",[AuthManager::class,"registerPost"])->name("register.post");
+Route::resource('events', EventController::class);
+//Route::resource('tickets', TicketController::class);
